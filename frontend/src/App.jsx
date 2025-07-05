b0739ff8b723d2c72addbe286ec6cd95d344c9a2
merrychi <merryamchitty@gmail.com>
2025-07-05 16:36:09 -0400

    eliminando carpetas innecesarias y configutaciones neuvas añadidas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,20 @@
+
 import { ConfigProvider } from 'antd';
 import esES from 'antd/locale/es_ES';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import './App.css'; 
+import './index.css';
 import AppRoutes from './router/AppRouter';
-
+import { tema } from './theme';
+import { AuthProvider } from './context/AuthContext'; 
 
 export default function App() {
   return (
-    <ConfigProvider
-      locale={esES}
-      theme={{
-        token: {
-          //Generales
-          borderRadius: 20,
-          fontSize: 16,
-          
-          // Colores informativos
-          colorPrimary: '#57d1c5',
-          colorSuccess: '#48bb78',         
-          colorWarning: '#ed8936',         
-          colorError: '#e53e3e',           
-          colorInfo: '#4299e1',           
-          colorBgLayout: '#ffffff',
-          
-          // Colores para el texto
-          colorText: '#333333',
-          colorTextSecondary: '#718096',   
-          colorTextDisabled: '#b0b8c4',
-
-          // Colores para fondo
-          colorBgLayout: '#e3f5f4',   
-          colorBgContainer: '#ffffff',
-        },
-        components: {
-          Button: {
-            colorPrimary: '#57d1c5',
-            borderRadius: 20,
-          },
-          Input: {
-            borderRadius: 20,
-          },
-          Card: {
-            borderRadius: 20,
-          },
-        },
-      }}
-    >
+    <ConfigProvider locale={esES} theme={tema}>
       <BrowserRouter>
-        <AppRoutes />
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
       </BrowserRouter>
     </ConfigProvider>
   );
 }
-
---
+++ frontend/src/theme.js
@@ -0,0 +1,32 @@
+export const tema = {
+  token: {
+    // --- Tokens Globales ---
+    borderRadius: 20,
+    fontSize: 16,
+
+
+    colorPrimary: '#376b83', // glacier-600
+    colorBgLayout: '#ffffff', // glacier-50
+    colorBgContainer: '#f1f8fa',
+    colorText: '#192933', // glacier-950
+    colorTextSecondary: '#32596c', // glacier-700
+
+    // --- Colores Semánticos ---
+    colorSuccess: '#22c55e',
+    colorWarning: '#f97316',
+    colorError: '#ef4444',
+    colorInfo: '#3b82f6',
+
+    // --- TOKEN PERSONALIZADO para hover en listas ---
+    colorItemHover: '#dcecf1', // glacier-100
+  },
+  
+
+  components: {
+
+    // --- Listas ---
+    List: {
+      itemPadding: '16px 24px', 
+    },
+  },
+};
